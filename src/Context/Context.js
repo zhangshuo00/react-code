7700f15c791edcf1074631576ea6aef3ea201121
zhangshuo <443494891@qq.com>
2019-10-27 17:25:41 +0800

    delete

--- src/Context/Context.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-export const con = React.createContext()
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Todolist from './Todolist/Todolist'
-
-
-ReactDOM.render(<Todolist />, document.getElementById('root'));
-
